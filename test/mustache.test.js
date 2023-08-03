b1cc04e03cd92fbf7b5a1d28b6b94d7aed7f629f
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-03 11:42:08 +0700

    Mustache Cache

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -4,3 +4,10 @@ test('Coba menggunakan mustache', () => {
   const data = Mustache.render(`Hello, {{name}}`, {name: 'Dzikri'});
   expect(data).toBe('Hello, Dzikri');
 });
+
+test('Coba menggunakan mustache Cache', () => {
+  Mustache.parse("Hello, {{name}}");
+  
+  const data = Mustache.render(`Hello, {{name}}`, {name: 'Dzikri'});
+  expect(data).toBe('Hello, Dzikri');
+});
